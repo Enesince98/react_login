643a9763bf6ecc0c3d67e27927146f29f97da658
Enesince98 <enes98lir@gmail.com>
2022-06-05 01:45:38 +0300

    Register page is added. Validation process is working but not token or username taken response is included.

---
+++ src/Register.js
@@ -0,0 +1,220 @@
+import React from "react";
+import { useRef, useState, useEffect } from "react";
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import axios from "./api/axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+const USER_REGEX = /^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+const PWD_REGEX =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
+const REGISTER_URL='/register';
+  const Register = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
+
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    const result = USER_REGEX.test(user);
+    console.log(result);
+    console.log(user);
+    setValidName(result);
+  }, [user]);
+
+  useEffect(() => {
+    const result = PWD_REGEX.test(pwd);
+    console.log(result);
+    console.log(pwd);
+    setValidPwd(result);
+    const match = pwd === matchPwd;
+    console.log(validMatch);
+  }, [pwd, matchPwd]);
+
+  useEffect(() => {
+    setValidPwd(PWD_REGEX.test(pwd));
+    setValidMatch(pwd === matchPwd);
+  }, [pwd, matchPwd]);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd, matchPwd]);
+
+  const handleSubmit = async (e)=>{
+      e.preventDefault();
+      try{
+          const response = await axios.post(REGISTER_URL,
+            JSON.stringify({user,pwd})/*user: userName*/,
+            {
+                headers:{'Content-Type':'application/json'},
+                withCredentials:true
+            }
+        );
+        console.log(response.data);
+        console.log(response.accessToken);
+        console.log(JSON.stringify(response))
+        setSuccess(true);
+      }
+      catch(err){
+          if(!err?.response){
+              setErrMsg('No Server Response');
+          }else if(err.response?.status === 409){
+              setErrMsg('Username Taken');
+          }else{
+              setErrMsg('Registration Failed')
+          }
+          errRef.current.focus()
+      }
+  }
+
+
+
+  return (
+    <>
+    {success ?(
+        <secction>
+            <h1>Success!</h1>
+            <p>
+                <a href="#">Sign In</a>
+            </p>
+        </secction>
+    ):(
+    <section>
+      <p
+        reg={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">
+          Username:
+          <span className={validName ? "valid" : "hide"}>
+            <FontAwesomeIcon icon={faCheck} />
+          </span>
+          <span className={validName || !user ? "hide" : "invalid"}>
+            <FontAwesomeIcon icon={faTimes} />
+          </span>
+        </label>
+        <input
+          type="text"
+          id="username"
+          ref={userRef}
+          autoComplete="off"
+          onChange={(e) => setUser(e.target.value)}
+          required
+          aria-invalid={validName ? "false" : "true"}
+          aria-describedby="uidnote"
+          onFocus={() => setUserFocus(true)}
+          onBlur={() => setUserFocus(false)}
+        />
+        <p
+          id="uidnote"
+          className={
+            userFocus && user && !validName ? "instructions" : "offscreen"
+          }
+        >
+          <FontAwesomeIcon icon={faInfoCircle} />
+          Username must be 8 to 20 characters long. Do not use underscore or dot
+          at the beginning or at the end. Only underscore and dot is allowed.
+        </p>
+
+        <label htmlFor="password">
+          Password:
+          <span className={validPwd ? "valid" : "hide"}>
+            <FontAwesomeIcon icon={faCheck} />
+          </span>
+          <span className={validPwd || !pwd ? "hide" : "invalid"}>
+            <FontAwesomeIcon icon={faTimes} />
+          </span>
+        </label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPwd(e.target.value)}
+          required
+          aria-invalid={validPwd ? "false" : "true"}
+          aria-describedby="pwdnote"
+          onFocus={() => setPwdFocus(true)}
+          onBlur={() => setPwdFocus(false)}
+        />
+        <p
+          id="pwdnote"
+          className={pwdFocus && !validPwd ? "instructions" : "offscreen"}
+        >
+          <FontAwesomeIcon icon={faInfoCircle} />
+          Password must be 8 to 20 characters. Must contain an uppercase, a
+          lowercase and a special character with a number. Allowed special
+          characters: <span aria-label="exclamation mark">!</span>
+          <span aria-label="at symbol">@</span>{" "}
+          <span aria-label="hashtag">#</span>
+          <span aria-label="dollar sign">$</span>{" "}
+          <span aria-label="percent">%</span>
+        </p>
+
+        <label htmlFor="confirm_pwd">
+          Confirm Password:
+          <span className={validMatch && matchPwd ? "valid" : "hide"}>
+            <FontAwesomeIcon icon={faCheck} />
+          </span>
+          <span className={validMatch || !matchPwd ? "hide" : "invalid"}>
+            <FontAwesomeIcon icon={faTimes} />
+          </span>
+        </label>
+        <input
+          type="password"
+          id="confirm_pwd"
+          onChange={(e) => setMatchPwd(e.target.value)}
+          required
+          aria-invalid={validMatch ? "false" : "true"}
+          aria-describedby="confirmNote"
+          onFocus={() => setMatchFocus(true)}
+          onBlur={() => setMatchFocus(false)}
+        />
+        <p
+          id="confirmNote"
+          className={matchFocus && !validMatch ? "instructions" : "offscreen"}
+        >
+          <FontAwesomeIcon icon={faInfoCircle} />
+          Passwords must match.
+        </p>
+
+        <button disabled = {!validName || !validPwd || !validMatch ? true: false}>
+        Sign Up
+        </button>
+      </form>
+      <p>
+          Already registered?<br/>
+          <span classNme="line">
+            {/*router link here */}
+              <a href="#">Sign In</a>
+          </span>
+      </p>
+    </section>
+    )}
+    </>
+  );
+};
+
+export default Register;
